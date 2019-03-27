32bbdf0d7befa1db1e2e109209f42d423d823103
lmcrazy <manlinux@sina.cn>
2019-03-27 14:36:59 +0800

    Find any position of a target number in a sorted array. Return -1 if target does not exis

--- src/main/java/com/javasuanfa/everyday/SearchNumber.java
+++ src/main/java/com/javasuanfa/everyday/SearchNumber.java
@@ -7,4 +7,30 @@ package com.javasuanfa.everyday;
  * @create: 2019-03-27 14:02
  **/
 public class SearchNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 4, 4};
+        int n = 3;
+        System.out.println(binarySearch(arr, 1));
+
+    }
+
+    public static int binarySearch(int[] array, int number) {
+        if (array == null)
+            return -1;
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) { // 考虑到两个相等的时候
+            int mid = low + (high - low) / 2;
+            if (array[mid] == number) {
+                return mid;
+            } else if (array[mid] > number) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        return -1;
+    }
 }
